d4ff06594c5a7426f6b32de244c57258685ab042
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2024-12-30 07:02:53 -0800

    added while loop for the posts in wp and also added single.php file for single post as per wp lookup for file names

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>Custom Theme</h1>
+<?php 
+    while (have_posts()) {
+        the_post(); ?>
+        <div>
+            <h2>
+                <a href="<?php the_permalink()?>">
+                    <?php the_title()?>
+                </a>
+            </h2>
+            <p><?php the_content()?></p>
+            <hr/>
+        </div>
+    <?php }
+?>
---
+++ single.php
@@ -0,0 +1,8 @@
+<div>
+    <h2>
+        <?php the_title()?>
+    </h2>
+    <p>
+        <?php the_content()?>
+    </p>
+</div>
