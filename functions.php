12eefcc4cc99546b20f7f6c918572da56688cd4f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-01 18:56:59 +0600

    added build css to functions.php

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 add_action('wp_enqueue_scripts', 'loadUniversityResources');
 
 function loadUniversityResources(){
-    wp_enqueue_style('university_main_css', get_stylesheet_uri());
+    wp_enqueue_style('university_main_css', get_theme_file_uri( '/build/style-index.css' ));
+    wp_enqueue_style('university_normalized_css', get_theme_file_uri( '/build/index.css' ));
 }
