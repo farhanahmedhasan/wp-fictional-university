f5ba3bba1c385903fbfea413c21b980e237e3d0a
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-01 19:04:51 +0600

    added fontawesome link

--- functions.php
+++ functions.php
@@ -5,4 +5,5 @@ add_action('wp_enqueue_scripts', 'loadUniversityResources');
 function loadUniversityResources(){
     wp_enqueue_style('university_main_css', get_theme_file_uri( '/build/style-index.css' ));
     wp_enqueue_style('university_normalized_css', get_theme_file_uri( '/build/index.css' ));
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 }
