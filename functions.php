754b5575188a7aa50582ba7fc03f7dee73f22952
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-02 11:34:59 +0600

    added titles for pages using actions in functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'loadUniversityResources');
+add_action( 'after_setup_theme', 'loadUniversityFeatures' );
 
 function loadUniversityResources(){
     wp_enqueue_style('university_normalized_css', get_theme_file_uri( '/build/index.css' ));
@@ -9,4 +10,8 @@ function loadUniversityResources(){
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
     wp_enqueue_script( 'main_js', get_theme_file_uri('/build/index.js'), ['jquery'], 1.0, true);
+}
+
+function loadUniversityFeatures(){
+    add_theme_support('title-tag');
 }
