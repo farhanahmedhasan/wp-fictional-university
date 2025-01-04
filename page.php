0e02fd7201f2826fe358966e3173055337a7b1d4
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-04 17:16:48 +0600

    worked with parent and child pages showed menus on parent page for child pages and breadcrumb on child pages also hiding menus where no child pages

--- page.php
+++ page.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php get_header(); 
+    //if true (has parent) shows parent id else evaluate to 0
+    $get_parent_page = wp_get_post_parent_id(get_the_ID());
+
+    $get_parent_link = get_permalink($get_parent_page);
+    $get_parent_title = get_the_title($get_parent_page);
+?>
 
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
@@ -10,23 +16,55 @@
       </div>
     </div>
 
+    
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
+    <?php 
+        if($get_parent_page){
+    ?>
+
+    <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href=<?php echo $get_parent_link ?>>
+            <i class="fa fa-home" aria-hidden="true"></i>
+                Back to <?php echo $get_parent_title ?>
+            </a> 
+            <span class="metabox__main"><?php the_title() ?></span>
         </p>
-      </div>
-
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div> -->
-
-      <div class="generic-content">
-        <?php the_content() ?>
-      </div>
     </div>
+
+    <?php } ?>
+
+    <?php 
+        // gives the list of ids, if what page we are in has child pages
+        // or gives 0 if current page we are in has no child
+        $childIDs = get_pages([
+            'child_of' => get_the_ID()
+        ]);
+
+        if($get_parent_page || $childIDs) { ?>
+            <div class="page-links">
+                <h2 class="page-links__title">
+                    <a href=<?php echo $get_parent_link ?>>
+                        <?php echo $get_parent_title ?>
+                    </a>
+                </h2>
+                <ul class="min-list">
+                    <?php 
+                        $id = $get_parent_page ? $id = $get_parent_page : $id = get_the_ID();
+
+                        wp_list_pages([
+                            'title_li' => null,
+                            'child_of' => $id,
+                            'sort_column' => 'menu_order'
+                        ]);
+                    ?>
+                </ul>
+            </div>
+
+    <?php } ?>
+
+    <div class="generic-content">
+        <?php the_content() ?>
+    </div>
+</div>
 <?php get_footer(); ?>
