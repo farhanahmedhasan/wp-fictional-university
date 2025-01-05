e8ef05f3f3b7a416f1bafc89e14dcd8b6e9c062f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-05 13:58:40 +0600

    added all the information and added category exceprt author date time all post related stuff

--- index.php
+++ index.php
@@ -11,7 +11,36 @@
 </div>
 
 <div class="container container--narrow page-section">
-    <h1>Posts</h1>
+    <?php
+        while(have_posts()){
+            the_post(); 
+    ?>
+    <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title">
+            <a href="<?php the_permalink() ?>">
+                <?php the_title() ?>
+            </a>
+        </h2>
+        <div class="metabox">
+            <p>
+                Posted by 
+                <?php the_author_posts_link()?> 
+                on 
+                <?php the_modified_date()?> 
+                at 
+                <?php the_modified_date( 'g:i a' ) ?> 
+                in
+                <?php the_category(', ') ?>
+            </p>
+        </div>
+        <div class="generic-content">
+            <p><?php the_excerpt() ?></p>
+            <p>
+                <a class="btn btn--blue" href="<?php the_permalink() ?>">Continue Reading &raquo;</a>
+            </p>
+        </div>
+    </div>
+    <?php } ?>
 </div>
 
 <?php get_footer() ?>
