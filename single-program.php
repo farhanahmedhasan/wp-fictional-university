da1582be3e86e9f1b7199377508ec3000ba96df5
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-15 12:45:42 +0600

    added all the events related to a program in a single-program page

--- single-program.php
+++ single-program.php
@@ -29,6 +29,52 @@
     <div class="generic-content">
         <?php the_content() ?>
     </div>
+
+    <?php 
+        $relatedEvents = new WP_Query([
+            'posts_per_page' => -1,
+            'post_type' => 'event',
+            'meta_query' => [
+                [
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => date('Y-m-d'),
+                ],
+                [
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_ID() . '"'
+                ]
+            ]
+        ]);    
+
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+        while($relatedEvents->have_posts()){
+            $relatedEvents->the_post();
+            $event_date = new DateTime(get_field('event_date'));
+    ?>
+            <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="">
+              <span class="event-summary__month"><?php echo $event_date->format('M'); ?></span>
+              <span class="event-summary__day"><?php echo $event_date->format('j'); ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny">
+                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+              </h5>
+              <p> 
+                <?php 
+                  $excerpt = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
+                  echo $excerpt;
+                ?> 
+                <a href="<?php the_permalink() ?>" class="nu gray">Read more</a>
+              </p>
+            </div>
+            </div>
+
+    <?php } ?>
 </div>
 
 <?php get_footer() ?>
