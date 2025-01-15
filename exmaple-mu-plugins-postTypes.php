b013bcb9c22cbe2bb7a2e6607ea4bd2bd5b26d8f
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-15 17:37:07 +0600

    added post type professor

--- exmaple-mu-plugins-postTypes.php
+++ exmaple-mu-plugins-postTypes.php
@@ -3,40 +3,45 @@ add_action('init', 'loadUniversityPostTypes');
 
 function loadUniversityPostTypes(){
     // Event Post Type
-    $eventLabels = [
-        'name'=> 'Events',
-        'singular_name' => 'Event',
-        'all_items' => 'All Events',
-        'add_new_item' => 'Add New Event',
-        'edit_item' => 'Edit Event',
-    ];
-
     register_post_type('event', [
         'show_in_rest' => true,
         'supports' => ['title', 'editor', 'excerpt'],
         'public' => true,
         'has_archive' => true,
-        'labels' => $eventLabels,
+        'labels' => getLabels('Events'),
         'menu_icon' => 'dashicons-calendar',
         'rewrite' => ['with_front' => false],
     ]);
 
     // Program Post Type
-    $programLabels = [
-        'name' => 'Programs',
-        'singular_name' => 'Program',
-        'all_items' => 'All Programs',
-        'add_new_item' => 'Add New Program',
-        'edit_item' => 'Edit Program',
-    ];
-
     register_post_type('program', [
         'show_in_rest' => true,
         'supports' => ['title', 'editor'],
         'public' => true,
         'has_archive' => true,
-        'labels' => $programLabels,
+        'labels' => getLabels('Programs'),
         'menu_icon' => 'dashicons-awards',
         'rewrite' => ['with_front' => false],
     ]);
+
+    // Professor Post Type
+    register_post_type('professor', [
+        'show_in_rest' => true,
+        'supports' => ['title', 'editor'],
+        'labels' => getLabels('Professors'),
+        'public' => true,
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'rewrite' => ['with_front' => false],
+    ]);
+}
+
+function getLabels($pluralName){
+    $singularName = substr($pluralName, 0, -1);
+    return [
+        'name' => $pluralName,
+        'singular_name' => $singularName,
+        'all_items' => "All $pluralName",
+        'add_new_item' => "Add New $singularName",
+        'edit_item' => "Edit $singularName",
+    ];
 }
