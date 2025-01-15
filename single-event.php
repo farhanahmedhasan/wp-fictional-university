ecd1627d8eb1ff5680ab792bcd8cdca941afb13e
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-15 11:22:19 +0600

    added related programs for a event in the front end

--- single-event.php
+++ single-event.php
@@ -34,6 +34,27 @@
     <div class="generic-content">
         <?php the_content() ?>
     </div>
+
+    <!-- Related Programs -->
+    <?php 
+        $relatedPrograms = get_field('related_programs');
+        
+        if($relatedPrograms){ ?>
+            <hr class="section-break">
+            <h2 class="headline headline--medium">Related Program(s)</h2>
+            <ul class="link-list min-list">
+                <?php
+                    foreach ($relatedPrograms as $program) {
+                ?>
+                        <li>
+                            <a href="<?php the_permalink($program) ?>">
+                                <?php echo $program->post_title ?>
+                            </a>
+                        </li>
+                        
+                <?php } ?>
+            </ul>
+    <?php } ?>
 </div>
 
 <?php get_footer() ?>
