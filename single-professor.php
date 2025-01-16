71dcbf8da6620816d0da056fe318fc79a555ef13
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-16 16:08:10 +0600

    added custom image size for page banner and also added custom fields pageBanner and page banner subtitle and used them in the single professor

--- single-professor.php
+++ single-professor.php
@@ -1,11 +1,13 @@
 <?php get_header() ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_field('page_banner_bg_image')['sizes']['PageBanner'] ?>)"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title() ?></h1>
         <div class="page-banner__intro">
-          <p>Don't forget to replace me later</p>
+            <p>
+                <?php the_field('page_banner_subtitle') ?>
+            </p>
         </div>
     </div>
 </div>
