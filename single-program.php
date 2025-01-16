bb6e93a12f170557705740d0edafa7dd7e099d9b
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-16 15:27:35 +0600

    used custom image size for the professors

--- single-program.php
+++ single-program.php
@@ -56,7 +56,7 @@
 			?>
 				<li class="professor-card__list-item">
 					<a class="professor-card" href="<?php the_permalink() ?>">
-						<img class="professor-card__image" src="<?php the_post_thumbnail_url() ?>" alt="">
+						<img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscaped') ?>" alt="">
 						<span class="professor-card__name"><?php the_title() ?></span>
 					</a>
 				</li>
