65be72407c012e4784082c0c2d5d0bbfd80fc590
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-18 15:37:30 +0600

    added component for event

--- single-program.php
+++ single-program.php
@@ -86,28 +86,9 @@
 
         while($relatedEvents->have_posts()){
             $relatedEvents->the_post();
-            $event_date = new DateTime(get_field('event_date'));
+            get_template_part('template-parts/content', 'event');
+        } 
     ?>
-            <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="">
-              <span class="event-summary__month"><?php echo $event_date->format('M'); ?></span>
-              <span class="event-summary__day"><?php echo $event_date->format('j'); ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny">
-                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-              </h5>
-              <p> 
-                <?php 
-                  $excerpt = has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);
-                  echo $excerpt;
-                ?> 
-                <a href="<?php the_permalink() ?>" class="nu gray">Read more</a>
-              </p>
-            </div>
-            </div>
-
-    <?php } ?>
 </div>
 
 <?php get_footer() ?>
