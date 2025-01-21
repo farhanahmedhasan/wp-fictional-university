b00825d7254d1a0b459507082a6d07e9e50ae23c
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-21 22:45:30 +0600

    fixed permalink for single-event page to events page

--- single-event.php
+++ single-event.php
@@ -8,7 +8,7 @@
              the_post(); 
         ?>
         <p>
-          <a class="metabox__blog-home-link" href=<?php echo get_post_type_archive_link('events') ?>>
+          <a class="metabox__blog-home-link" href=<?php echo site_url('/events') ?>>
             <i class="fa fa-home" aria-hidden="true"></i>
                 Events Home
             </a> 
