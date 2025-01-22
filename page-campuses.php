ae2441e59417f0ab20046b66241efa21ece3091e
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-23 00:59:09 +0600

    added open street map frontend

---
+++ page-campuses.php
@@ -0,0 +1,33 @@
+<?php
+    get_header();
+    get_template_part('template-parts/banner');
+?>
+
+<div class="container container--narrow page-section">
+    <!-- Campuses -->
+    <div class="acf-map">
+        <?php
+        $query = new WP_Query([
+                'posts_per_page' => -1,
+                'post_type' => 'campus',
+                'order' => 'ASC'
+        ]);
+
+        while($query->have_posts()){
+            $query->the_post();
+
+            $campusLocation = get_field('campus_location');
+            $lat = $lng = $campusLocation['lat'];
+            $lng = $campusLocation['lng']
+            ?>
+            <div class="marker" data-lat="<?php echo $lat ?>" data-lng="<?php echo $lng ?>"></div>
+        <?php } ?>
+    </div>
+
+
+    <!-- Pagination -->
+    <!-- TODO: Change pagination query style /?page=1 not /page/1/  -->
+    <?php echo paginate_links() ?>
+</div>
+
+<?php get_footer() ?>
