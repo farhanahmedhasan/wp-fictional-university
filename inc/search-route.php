e68e90937c44755986345c37d7c931c9daa11fef
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-01-26 16:00:16 +0600

    Inhanced our custom search api so that it returns all professors customized data

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,15 +2,36 @@
 
 add_action('rest_api_init', 'registerSearch');
 
-function registerSearch(): void
-{
+function registerSearch(): void {
     register_rest_route('university/v1', 'search', [
         'methods' => WP_REST_Server::READABLE, // This returns to 'GET' (benefits if any web host uses slightly different value)
         'callback' => 'getSearchResults'
     ]);
 }
 
-function getSearchResults()
-{
-    return 'Congratulation You Created a ROUTE';
+function getSearchResults(): array {
+    return [
+        'cat' => 'meow',
+        'professors' => getProfessors()
+    ];
+}
+
+function getProfessors (): array {
+    $professorQuery = new WP_Query([
+        'post_type' => 'professor',
+        'posts_per_page' => -1
+    ]);
+
+    $professors = [];
+
+    while ($professorQuery->have_posts()){
+        $professorQuery->the_post();
+
+        $professors[] = [
+            'title' => get_the_title(),
+            'link' => get_the_permalink()
+        ];
+    }
+
+    return $professors;
 }
