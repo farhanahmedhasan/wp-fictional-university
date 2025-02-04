3ef2213a24034383d8643af732bf7cf816cd851d
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-02-04 18:13:23 +0600

    added dynamic login and registration routes using wp function

--- header.php
+++ header.php
@@ -40,8 +40,8 @@
               <?php }
 
               if (!is_user_logged_in()){ ?>
-                  <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-                  <a href="<?php echo esc_url(site_url('/wp-signup.php'))?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                  <a href="<?php echo wp_login_url() ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
+                  <a href="<?php echo wp_registration_url() ?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
             <?php  }
             ?>
             <a href="<?php echo esc_url(site_url('/search')) ?>" class="search-trigger js-search-trigger">
