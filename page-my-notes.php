9137a2e5771bb064568638df42fdac10ed60de06
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2025-02-07 19:19:23 +0600

    showed all notes based on user that is currently logged in

--- page-my-notes.php
+++ page-my-notes.php
@@ -9,6 +9,27 @@
 ?>
 
   <div class="container container--narrow page-section">
-    Custom notes
+    <ul id="my-notes" class="min-list link-list">
+        <!-- TODO: White space on textarea-->
+        <?php
+            $user_notes = new WP_Query([
+              'post_type' => 'notebook',
+              'posts_per_page' => -1,
+              'author' => get_current_user_id()
+            ]);
+            
+            while ($user_notes->have_posts()) {
+                $user_notes->the_post(); ?>
+                <li>
+                    <input class="note-title-field" value="<?php echo esc_attr(get_the_title()) ?>"/>
+                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
+                    <textarea class="note-body-field">
+                      <?php echo esc_textarea(wp_strip_all_tags(get_the_content(), true)); ?>
+                    </textarea>
+                </li>
+            <?php }
+        ?>
+    </ul>
   </div>
 <?php get_footer(); ?>
